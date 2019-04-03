c8b7961c49ec67331e618e62062882c8da199e28
Akbar Viji <Akbar Viji@AKBARVIJI.mshome.net>
2019-04-03 21:32:10 +0530

    completed successfully

--- src/main/java/main/Calculator.java
+++ src/main/java/main/Calculator.java
@@ -38,26 +38,25 @@ public class Calculator {
     public static int add(int x, int y)
     {
     	int result=0;
-//        write your code here 
-    	
+        result = x+y;    	
         return result;
     }
     public static int sub(int x, int y)
     {
     	int result=0;
-//        write your code here
+        result = x-y;    	
         return result;
     }
     public static int mult(int x, int y)
     {
     	int result=0;
-//      write your code here        
+        result = x*y;    	
     	return result;
     }
     public static int div(int x, int y)
     {
     	int result=0;
-//      write your code here
+        result = x/y;    	
     	return result;
     }
 
